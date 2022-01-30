ad6da64cc11a17816a32dd6de703fa32ae6c37e2
Rod <rod@Rods-MacBook-Pro.local>
2022-01-30 00:15:55 -0300

    first commit

---
+++ desafiopokeguia/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <ul>
+      <li>charmander</li>
+      <li>charizard</li>
+      <li>wartortle</li>
+      <li>caterpie</li>
+      <li>butterfree</li>
+      <li>kakuna</li>
+      <li>pidgey</li>
+      <li>pidgeot</li>
+      <li>raticate</li>
+    </ul>
+
+    <img class="poke_logo" src="./assets/pokemon_logo.png" alt="pokemon logo" />
+    <section>
+      <input type="text" v-model="nameInputbyUserPokemon007" />
+      <button
+        type="submit"
+        @click.prevent="passInputNameToGetDataFromApiMethod007"
+      >
+        Asignar Input a URL para llamar API
+      </button>
+    </section>
+    <PokemonObjectAttributeComponent007
+      :attributesPokemon007="attributesPokemon007"
+    />
+  </div>
+</template>
+
+<script>
+import PokemonObjectAttributeComponent007 from "@/components/PokemonObjectAttributeComponent";
+import { getDataFromApiPokemon007 } from "./api/ApiUrlFetchPokemon007";
+export default {
+  name: "App",
+  components: {
+    PokemonObjectAttributeComponent007,
+  },
+  data() {
+    return {
+      nameInputbyUserPokemon007: "",
+      attributesPokemon007: {},
+    };
+  },
+  methods: {
+    async passInputNameToGetDataFromApiMethod007() {
+      let { nameInputbyUserPokemon007 } = this; // captura en la variable el texto ingresado por el usuario
+      this.attributesPokemon007 = await getDataFromApiPokemon007(
+        nameInputbyUserPokemon007
+      );
+      console.log(nameInputbyUserPokemon007);
+    },
+  },
+  async created() {
+    this.attributesPokemon007 = await getDataFromApiPokemon007("pikachu");
+  },
+};
+</script>
+
+<style></style>
---
+++ desafiopokeguia/src/components/PokemonObjectAttributeComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <img :src="attributesPokemon007.imagen" />
+    <h1>Habilidades</h1>
+    <section>
+      <div
+        v-for="(skillArgument007, index007) in attributesPokemon007.skills"
+        :key="index007"
+      >
+        <p>{{ skillArgument007.ability.name }}</p>
+      </div>
+    </section>
+    <h1>Movimientos</h1>
+    <section class="container-movements">
+      <div
+        v-for="(movements, index) in attributesPokemon007.movements"
+        :key="index"
+      >
+        <p>{{ movements.move.name }}</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PokemonObjectAttributeComponent007",
+  props: {
+    attributesPokemon007: {},
+  },
+};
+</script>
+<style></style>
